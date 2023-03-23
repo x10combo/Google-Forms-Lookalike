8efe390db9611216acfa15ec516f4f2d0a9117dd
rakel <rakelspahi03@gmail.com>
2023-03-23 09:36:03 +0100

    no message

--- connect.php
+++ connect.php
@@ -1,3 +1,8 @@
 <?php
-
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$name = $_POST['trainername'];
+$langselect = $_POST['langselect'];
+$feedback = $_POST['feedback'];
 ?>
