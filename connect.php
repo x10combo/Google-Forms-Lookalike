b9860e905e34d3b6009e3db7e91f57c6ee4d6716
rakel <rakelspahi03@gmail.com>
2023-03-23 09:48:51 +0100

    no message

--- connect.php
+++ connect.php
@@ -2,7 +2,21 @@
 $name = $_POST['name'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
-$name = $_POST['trainername'];
+$trainername = $_POST['trainername'];
 $langselect = $_POST['langselect'];
 $feedback = $_POST['feedback'];
+
+
+
+
+$conn = new mysqli('localhost', 'root', '', 'registration_form');
+if($conn->connect_error){
+    die('Connection Failed  :  ' $conn->connect_error);
+}
+else{
+    $stmt - $conn->prepare("Insert into registration(name, lastname, email, langselect, feedback
+}
+
+
 ?>
+
