db6d660a0e389ec2a272d1bae17ca521121edeee
rakel <rakelspahi03@gmail.com>
2023-03-24 12:28:45 +0100

    Lines do not respond as planned. Fixing needed.

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ rating.addEventListener("click",function(){
 
 function vote () {
 }
+
+
+// Set a random position for the left and right lines
+var windowHeight = window.innerHeight;
+var leftPosition = Math.random() * (windowHeight - 100) + 50; // range from 50 to windowHeight - 50
+var rightPosition = Math.random() * (windowHeight - 100) + 50; // range from 50 to windowHeight - 50
+document.querySelector('.left').style.top = leftPosition + 'px';
+document.querySelector('.right').style.top = rightPosition + 'px';
+
+// Set a random delay for the left and right lines
+var leftDelay = Math.random() * 4 + 1; // range from 1 to 5
+var rightDelay = Math.random() * 4 + 1; // range from 1 to 5
+document.querySelector('.left').style.animationDelay = '-' + leftDelay + 's';
+document.querySelector('.right').style.animationDelay = '-' + rightDelay + 's';
